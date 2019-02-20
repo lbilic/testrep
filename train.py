e6ae069706511547ef8fc8cc8039dafacb8f03f8
Luka Bilic <phoenix-023@hotmail.com>
2019-02-20 18:55:44 +0100

    full

---
+++ train.py
@@ -0,0 +1,104 @@
+from __future__ import print_function
+
+import os
+from skimage.transform import resize
+from skimage.io import imsave
+import numpy as np
+from keras.models import Model
+from keras.layers import Input, concatenate, Conv2D, MaxPooling2D, Conv2DTranspose
+from keras.optimizers import Adam
+from keras.callbacks import ModelCheckpoint
+from keras import backend as K
+
+from data import load_train_data, load_test_data
+from model import unet
+
+K.set_image_data_format('channels_last')  # TF dimension ordering in this code
+
+smooth = 1.
+
+
+def dice_coef(y_true, y_pred):
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+
+
+def dice_coef_loss(y_true, y_pred):
+    return -dice_coef(y_true, y_pred)
+
+def preprocess(imgs):
+    imgs_p = np.ndarray((imgs.shape[0], 256, 256), dtype=np.uint8)
+    for i in range(imgs.shape[0]):
+        imgs_p[i] = resize(imgs[i], (256, 256), preserve_range=True)
+
+    imgs_p = imgs_p[..., np.newaxis]
+    return imgs_p
+
+
+def train_and_predict():
+    print('-'*30)
+    print('Loading and preprocessing train data...')
+    print('-'*30)
+    imgs_train, imgs_mask_train = load_train_data()
+
+    imgs_train = preprocess(imgs_train)
+    imgs_mask_train = preprocess(imgs_mask_train)
+
+    imgs_train = imgs_train.astype('float32')
+    mean = np.mean(imgs_train)  # mean for data centering
+    std = np.std(imgs_train)  # std for data normalization
+
+    imgs_train -= mean
+    imgs_train /= std
+
+    imgs_mask_train = imgs_mask_train.astype('float32')
+    imgs_mask_train /= 255.  # scale masks to [0, 1]
+
+    print('-'*30)
+    print('Creating and compiling model...')
+    print('-'*30)
+    model = unet()
+    model_checkpoint = ModelCheckpoint('weights.h5', monitor='val_loss', save_best_only=True)
+
+    print('-'*30)
+    print('Fitting model...')
+    print('-'*30)
+    model.fit(imgs_train, imgs_mask_train, batch_size=32, nb_epoch=20, verbose=1, shuffle=True,
+              validation_split=0.2,
+              callbacks=[model_checkpoint])
+
+    print('-'*30)
+    print('Loading and preprocessing test data...')
+    print('-'*30)
+    imgs_test, imgs_id_test = load_test_data()
+    imgs_test = preprocess(imgs_test)
+
+    imgs_test = imgs_test.astype('float32')
+    imgs_test -= mean
+    imgs_test /= std
+
+    print('-'*30)
+    print('Loading saved weights...')
+    print('-'*30)
+    model.load_weights('weights.h5')
+
+    print('-'*30)
+    print('Predicting masks on test data...')
+    print('-'*30)
+    imgs_mask_test = model.predict(imgs_test, verbose=1)
+    np.save('imgs_mask_test.npy', imgs_mask_test)
+
+    print('-' * 30)
+    print('Saving predicted masks to files...')
+    print('-' * 30)
+    pred_dir = 'preds'
+    if not os.path.exists(pred_dir):
+        os.mkdir(pred_dir)
+    for image, image_id in zip(imgs_mask_test, imgs_id_test):
+        image = (image[:, :, 0] * 255.).astype(np.uint8)
+        imsave(os.path.join(pred_dir, str(image_id) + '_pred.png'), image)
+
+if __name__ == '__main__':
+    train_and_predict()
