fff552d633a207953059a0d51cfc9202e454c2d8
Luka Bilic <phoenix-023@hotmail.com>
2019-02-23 08:50:16 +0100

    model

--- model.py
+++ model.py
@@ -63,7 +63,7 @@ def unet(pretrained_weights = None,input_size = (256,256,1)):
     conv9 = Conv2D(64, (3, 3), activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge9)
     conv9 = Conv2D(64, (3, 3), activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
     conv9 = Conv2D(2, (3, 3), activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
-    conv10 = Conv2D(1, (1, 1), activation = 'sigmoid')(conv9)
+    conv10 = Conv2D(1, (1, 1), name='weight_output')(conv9)
 
     model = Model(inputs = [inputs], outputs = [conv10])
 
